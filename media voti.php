658ef6166ffddb21dadd03b7af172b218846e24a
AndreaMontinovo <32158405+AndreaMontinovo@users.noreply.github.com>
2017-09-21 10:27:14 +0200

    Add files via upload

---
+++ media voti.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Media dei voti</title>
+</head>
+<body>
+<?php
+/*
+* calcolo della media dei voti
+*/
+$voti = array(7,7,6,6,7,7,8,8,7);
+$limite = count($voti);
+$i=0;
+$somma=0;
+while ($i<$limite)
+{
+$somma += $voti[$i];
+$i++;
+}
+$media=$somma/$limite;
+echo "<h2>Media dei voti: " . $media . "</h2>";
+?>
+</body>
+</html>
